23d7ccb5901ff7d16eb594b731058a52ac98fa49
t-craiw <william.craig@autodesk.com>
2021-08-10 00:12:04 -0700

    first commit

---
+++ test/page-objects/BasePage.js
@@ -0,0 +1,34 @@
+import {DEFAULT_TIMEOUT} from '../configs/e2eConstants';
+require('dotenv').config();
+
+
+export default class BasePage {
+    constructor(selector) {
+        this.selector = selector;
+    }
+
+    /**
+     * Wait for the element to be displayed
+     *
+     * @return {boolean}
+     */
+    waitForIsShown(isShown = true) {
+        try{
+            return $(this.selector).waitForDisplayed({
+                timeout: DEFAULT_TIMEOUT,
+                reverse: !isShown
+            });
+        } catch (e) {
+            return !isShown;
+        }
+    }
+
+    /**
+     * Give back if the element is displayed
+     *
+     * @return {boolean}
+     */
+    isDisplayed() {
+        return $(this.selector).isDisplayed();
+    }
+}
